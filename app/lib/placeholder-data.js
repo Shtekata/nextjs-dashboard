6928092bc5ba38d6177bddd0a47ec1ab09dbc5a1
Shtekata <gesheval@gmail.com>
2024-02-24 18:15:15 +0200

    Seed db

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -7,6 +7,18 @@ const users = [
     email: 'user@nextmail.com',
     password: '123456',
   },
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442b',
+    name: 'Mser',
+    email: 'mser@nextmail.com',
+    password: '123456',
+  },
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442c',
+    name: 'Nser',
+    email: 'nser@nextmail.com',
+    password: '123456',
+  },
 ];
 
 const customers = [

826fd0ef2fe75c4a73794d34dc8dd4925f2c8f56
Shtekata <gesheval@gmail.com>
2024-03-09 14:26:06 +0200

    Add some data in dashboard/page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -25,7 +25,8 @@ export async function fetchRevenue() {
 
     // console.log('Data fetch completed after 3 seconds.');
 
-    return data.rows;
+    return data.
+      rows;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');

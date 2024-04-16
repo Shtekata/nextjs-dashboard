35835112f8af779367703056448690481f537762
Shtekata <gesheval@gmail.com>
2024-04-16 19:47:41 +0300

    Adding page titles

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,6 +1,9 @@
 import Form from '@/app/ui/invoices/create-form'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import { fetchCustomers } from '@/app/lib/data'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = { title: 'Create Invoice' }
 
 export default async function Page() {
   const customers = await fetchCustomers()
@@ -10,7 +13,11 @@ export default async function Page() {
       <Breadcrumbs
         breadcrumbs={[
           { label: 'Invoices', href: '/dashboard/invoices' },
-          { label: 'Create Invoice', href: '/dashboard/invoices/create', active: true }
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true
+          }
         ]}
       />
       <Form customers={customers} />
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,9 @@
 import SideNav from '@/app/ui/dashboard/sidenav'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: { template: '%s | Asen Dashboard', default: 'Asen Dashboard' }
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (

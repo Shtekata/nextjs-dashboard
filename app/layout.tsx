fd41694443c2cb06598b4119ce36f5a54c427bf2
Shtekata <gesheval@gmail.com>
2024-04-16 14:40:29 +0300

    Add metadataBase

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,3 +12,21 @@ export default function RootLayout({
     </html>
   )
 }
+
+const DEV = process.env.METADATABASEURLDEV
+const PROD = process.env.METADATABASEURLPROD
+const url = String(DEV ? DEV : PROD)
+
+export const metadata = {
+  metadataBase: new URL(url),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+      'de-DE': '/de-DE'
+    }
+  },
+  openGraph: {
+    images: '/opengraph-image.png'
+  }
+}

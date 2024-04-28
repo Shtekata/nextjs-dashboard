b60d0d32dfdb49ce0ed4848349e776cf3ae0b842
Shtekata <gesheval@gmail.com>
2024-04-28 14:43:17 +0300

    Add some style

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,19 @@ import AcmeLogo from '@/app/ui/acme-logo'
 import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import styles from '@/app/ui/home.module.css'
-import { lusitana } from '@/app/ui/fonts'
+import { lusitana, roboto_mono, roboto, jacques, noto } from '@/app/ui/fonts'
 import Image from 'next/image'
+import icons from '@/app/ui/icons.module.css'
+import utils from '@/app/ui/utils.module.css'
+import knuckles from '@/public/knuckles.gif'
+import sponge_bob from '@/public/sponge_bob.gif'
+import rice_bowl from '@/public/rice_bowl.png'
+import loading from '@/public/loading.svg'
 
 export default function Page() {
+  const iconsArr = `${icons.clock} ${icons.smile}`
+  const smile = { __html: String.fromCodePoint(0x1f60a) }
+  const timeRot = { ['--time' as string]: '5s' }
   return (
     <main className='flex min-h-screen flex-col p-6'>
       <div className='flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52'>
@@ -13,8 +22,61 @@ export default function Page() {
       </div>
       <div className='mt-4 flex grow flex-col gap-4 md:flex-row'>
         <div className='flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20'>
-          {/* <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"/> */}
+          {/* <div className='h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent' /> */}
           <div className={styles.shape} />
+          <p className={`${roboto_mono.className}`}>Ala_Bala</p>
+          <p className={`${lusitana.className}`}>Ala_Bala</p>
+          <div className='flex'>
+            <p
+              className={`${roboto.className} m-auto font-thin text-red-900 text-xl`}
+            >
+              Ala_Bala
+            </p>
+            <p
+              className={`${roboto.className} m-auto font-medium text-green-900 text-2xl`}
+            >
+              Ala_Bala
+            </p>
+            <p
+              className={`${jacques.className} m-auto font-black text-blue-800 text-3xl`}
+            >
+              Ala_Bala
+            </p>
+            <p
+              className={`${noto.className} m-auto font-black text-blue-800 text-3xl`}
+            ></p>
+          </div>
+          <div className='flex justify-center'>
+            <span className={iconsArr} />
+            <span className={icons.hamburger} />
+            <span dangerouslySetInnerHTML={smile} />
+            <>{String.fromCodePoint(0x1f354)}</>
+            <>{String.fromCodePoint(0x1f4af)}</>
+            <>{String.fromCodePoint(0x1f383)}</>
+          </div>
+          <div className='flex justify-between'>
+            <Image src={knuckles} alt='knuckles' width={25} />
+            <Image
+              className={utils.rotate}
+              style={timeRot}
+              src={rice_bowl}
+              alt='rice_bowl'
+              width={25}
+            />
+            <Image src={sponge_bob} alt='sponge_bob' width={25} />
+          </div>
+          <button
+            type='button'
+            className='flex items-center gap-5 self-center rounded-xl bg-indigo-500 px-6 py-3 text-base font-semibold text-white transition-colors hover:bg-pink-700 md:text-lg'
+          >
+            <Image
+              src={loading}
+              alt='loading animation'
+              width={20}
+              height={20}
+            />
+            Processing
+          </button>
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,29 @@
-import { Inter,Lusitana } from 'next/font/google'
+import {
+  Inter,
+  Lusitana,
+  Roboto_Mono,
+  Roboto,
+  Jacques_Francois_Shadow,
+  Noto_Color_Emoji
+} from 'next/font/google'
 export const inter = Inter({ subsets: ['latin'] })
-export const lusitana = Lusitana({weight:['400','700'],subsets:['latin']})
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] })
+export const roboto_mono = Roboto_Mono({ subsets: ['latin'], display: 'swap' })
+export const roboto = Roboto({
+  weight: ['100', '500', '900'],
+  style: ['italic'],
+  subsets: ['latin'],
+  display: 'swap'
+})
+export const jacques = Jacques_Francois_Shadow({
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap'
+})
+export const noto = Noto_Color_Emoji({
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['emoji'],
+  display: 'swap'
+})
